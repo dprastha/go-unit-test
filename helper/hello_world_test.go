1a0d116168d584c9058c45455149a2dfdf31fb6f
dprastha <danielprastha4@gmail.com>
2022-01-03 14:52:24 +0700

    using testify

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,7 +3,22 @@ package helper
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
+
+func TestHelloWorldRequire(t *testing.T) {
+	result := HelloWorld("Clow")
+	require.Equal(t, "Hello Clo", result, "Result must be "+result)
+	fmt.Println("TestHelloWorld with Assert done")
+}
+
+func TestHelloWorldAssert(t *testing.T) {
+	result := HelloWorld("Daniel")
+	assert.Equal(t, "Hello Danie", result, "Result must be "+result)
+	fmt.Println("TestHelloWorld with Assert done")
+}
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Daniel")
