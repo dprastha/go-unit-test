71b87c1d6e2ea37c634ef093bfffa7ca9800e707
dprastha <danielprastha4@gmail.com>
2022-01-03 14:43:56 +0700

    go testing module

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,11 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T) {
 	result := HelloWorld("Daniel")
 
 	if result != "Hello Daniel" {
-		panic("Result must be 'Hello Daniel'")
+		t.Error("Result must be 'Hello Daniel'")
 	}
+
+	fmt.Println("TestHelloWorld done")
+}
+
+func TestHelloWorldRoach(t *testing.T) {
+	result := HelloWorld("Roac")
+
+	if result != "Hello Roach" {
+		t.Error("Result must be 'Hello Roach'")
+	}
+
+	fmt.Println("TestHelloWorldRoach done")
 }
