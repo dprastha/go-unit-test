9bca2902f5cf85ca57390057ffc2035ffd14e494
dprastha <danielprastha4@gmail.com>
2022-01-04 09:44:18 +0700

    sub test

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,27 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestSubTest(t *testing.T) {
+	t.Run("Daniel", func(t *testing.T) {
+		result := HelloWorld("Daniel")
+		assert.Equal(t, "Hello Daniel", result, "Result must be "+result)
+	})
+	t.Run("Miu", func(t *testing.T) {
+		result := HelloWorld("Miu")
+		assert.Equal(t, "Hello Miu", result, "Result must be "+result)
+	})
+}
+
+func TestMain(m *testing.M) {
+	// Before
+	fmt.Println("Before Unit Test")
+
+	m.Run()
+
+	// After
+	fmt.Println("After Unit Test")
+}
+
 func TestSkip(t *testing.T) {
 	if runtime.GOOS == "linux" {
 		t.Skip("Cannot run on Linux")
@@ -16,18 +37,17 @@ func TestSkip(t *testing.T) {
 
 	result := HelloWorld("Clow")
 	require.Equal(t, "Hello Clow", result, "Result must be "+result)
-
 }
 
 func TestHelloWorldRequire(t *testing.T) {
 	result := HelloWorld("Clow")
-	require.Equal(t, "Hello Clo", result, "Result must be "+result)
+	require.Equal(t, "Hello Clow", result, "Result must be "+result)
 	fmt.Println("TestHelloWorld with Assert done")
 }
 
 func TestHelloWorldAssert(t *testing.T) {
 	result := HelloWorld("Daniel")
-	assert.Equal(t, "Hello Danie", result, "Result must be "+result)
+	assert.Equal(t, "Hello Daniel", result, "Result must be "+result)
 	fmt.Println("TestHelloWorld with Assert done")
 }
 
@@ -42,7 +62,7 @@ func TestHelloWorld(t *testing.T) {
 }
 
 func TestHelloWorldRoach(t *testing.T) {
-	result := HelloWorld("Roac")
+	result := HelloWorld("Roach")
 
 	if result != "Hello Roach" {
 		t.Error("Result must be 'Hello Roach'")
